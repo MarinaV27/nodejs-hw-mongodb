d7b2fbce38584e07703100050c0775bfd8432a95
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-24 21:55:01 +0200

    add PORT in servers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+  },
 ];
