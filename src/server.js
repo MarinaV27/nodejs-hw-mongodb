458f8cc4aaa5836ddc8945718c77a17ebb05361f
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-26 20:46:32 +0200

    add in server.js app.get

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -31,6 +32,33 @@ export const setupServer = () => {
     app.use('*', (req, res) => {
         res.status(404).json({
             message: 'Not found',
+        });
+    });
+
+
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res, next) => {
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+
+        // Відповідь, якщо контакт не знайдено
+        if (!contact) {
+            res.status(404).json({
+                message: 'Student not found'
+            });
+            return;
+        }
+
+        // Відповідь, якщо контакт знайдено
+        res.status(200).json({
+            data: contact,
         });
     });
 
