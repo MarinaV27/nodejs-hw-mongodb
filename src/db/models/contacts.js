4f0479a9c78f829fbe89e0b711913e063f012322
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-26 19:41:47 +0200

    add contacts.js in db/models

---
+++ src/db/models/contacts.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+name - string, required
+phoneNumber - string, required
+email - string
+isFavourite - boolean, default false
+contactType - string, enum(’work’, ‘home’, ‘personal’), required, default ‘personal’
