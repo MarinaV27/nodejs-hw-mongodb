5977baa7d184a4560ffb3e34e6e2ac7f90f73b73
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-26 19:57:03 +0200

    add contactsShema

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,16 +1,28 @@
+import { model, Schema } from "mongoose";
+
+const contactsShema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        isFavourite: {
+            type: Boolean,
+            default: false,
+        },
+        contactType: {
+            type: String,
+            enam: ['work', 'home', 'personal'],
+            required: true,
+            default: 'personal',
+        },
+    },
+);
+
+export const ContactsCollection = model('contacts', contactsShema);
 
 
-
-
-
-
-
-
-
-
-
-name - string, required
-phoneNumber - string, required
-email - string
-isFavourite - boolean, default false
-contactType - string, enum(’work’, ‘home’, ‘personal’), required, default ‘personal’
