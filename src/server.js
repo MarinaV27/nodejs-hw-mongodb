2d85272e177240ae326bb07afff4043b8def223b
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-26 21:01:41 +0200

    all

--- src/server.js
+++ src/server.js
@@ -29,36 +29,40 @@ export const setupServer = () => {
         });
     });
 
-    app.use('*', (req, res) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
-
-
     app.get('/contacts', async (req, res) => {
         const contacts = await getAllContacts();
 
         res.status(200).json({
+            status: 200,
+            message: "Successfully created a contact!",
             data: contacts,
         });
     });
 
-    app.get('/contacts/:contactId', async (req, res, next) => {
+    app.get('/contacts/:contactId', async (req, res) => {
         const { contactId } = req.params;
         const contact = await getContactById(contactId);
 
         // Відповідь, якщо контакт не знайдено
         if (!contact) {
             res.status(404).json({
-                message: 'Student not found'
+                status: 404,
+                message: 'Contact not found',
             });
             return;
         }
 
         // Відповідь, якщо контакт знайдено
         res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}`,
             data: contact,
+        });
+    });
+
+    app.use('*', (req, res) => {
+        res.status(404).json({
+            message: 'Not found',
         });
     });
 
