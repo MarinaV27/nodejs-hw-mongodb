8e56c98708f3f4790a502e4f6571fcdd5279bd9e
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-27 22:41:25 +0200

    add ctrlWrapper.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,15 +1,20 @@
 import { getAllContacts, getContactById } from "../services/contacts.js";
 
 
-export const getContactsController = async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
+export const getContactsController = async (req, res, next) => {
+    try {
+        const contacts = await getAllContacts();
+        res.status(200).json({
         status: 200,
         message: "Successfully created a contact!",
         data: contacts,
     });
+    } catch (error) {
+        next(error);
+    }
+    
 };
+
     export const getContactsByIdController = async (req, res, next) => {
         const { contactId } = req.params;
         const contact = await getContactById(contactId);
