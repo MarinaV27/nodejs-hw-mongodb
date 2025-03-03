426f0baaa569e75e0889017c7af56e9576c34eed
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-03 22:05:58 +0200

    add const updateContactSchema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,14 +3,21 @@ import Joi from "joi";
 export const createContactSchema = Joi.object(
     {
         name: Joi.string().min(3).max(30).required(),
-        phoneNumber: Joi.string().min(3).max(20).pattern(/[+0*9]{3,20}$/),
-        email: Joi.string().min(3).max(20).required(),
+        phoneNumber: Joi.string().min(3).max(20).required().pattern(/[+0*9]{3,20}$/),
+        email: Joi.string().min(3).max(20),
         isFavourite: Joi.boolean(),
         contactType: Joi.string().min(3).max(20).required(),
 
     });
 
-
+export const updateContactSchema = Joi.object(
+    {
+        name: Joi.string().min(3).max(30),
+        phoneNumber: Joi.string().min(3).max(20).pattern(/[+0*9]{3,20}$/),
+        email: Joi.string().min(3).max(20),
+        isFavourite: Joi.boolean(),
+        contactType: Joi.string().min(3).max(20),
+    });
 
 
 
