81bcfd601c2dd930ebafc711118945a69ad1ea79
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-03 22:11:32 +0200

    add validateBody in router.patch

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,7 +8,7 @@ import {
 } from "../controllers/contacts.js"; 
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
-import { createContactSchema } from "../validation/contacts.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 
 const router = express.Router();
 const jsonParser = express.json();
@@ -19,7 +19,7 @@ router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
 router.post('/contacts', jsonParser, validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.patch('/contacts/:contactId', jsonParser, validateBody(createContactSchema), ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', jsonParser, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
