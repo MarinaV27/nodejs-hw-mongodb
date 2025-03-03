d4c27351ead234e84ff01d5e85ec53d74a6f1250
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-03 21:55:37 +0200

    add validateBody in router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,6 +7,8 @@ import {
     deleteContactController
 } from "../controllers/contacts.js"; 
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { createContactSchema } from "../validation/contacts.js";
 
 const router = express.Router();
 const jsonParser = express.json();
@@ -15,9 +17,9 @@ router.get('/contacts', ctrlWrapper(getContactsController));
     
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
-router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
+router.post('/contacts', jsonParser, validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.patch('/contacts/:contactId', jsonParser, ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', jsonParser, validateBody(createContactSchema), ctrlWrapper(patchContactController));
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,12 +1,12 @@
 import Joi from "joi";
 
-export const contactSchema = Joi.object(
+export const createContactSchema = Joi.object(
     {
         name: Joi.string().min(3).max(30).required(),
         phoneNumber: Joi.string().min(3).max(20).pattern(/[+0*9]{3,20}$/),
-        email: Joi.string().required(),
+        email: Joi.string().min(3).max(20).required(),
         isFavourite: Joi.boolean(),
-        contactType: Joi.string().required(),
+        contactType: Joi.string().min(3).max(20).required(),
 
     });
 
