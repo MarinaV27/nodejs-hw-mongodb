f82673dc3279d865b43e740bdd0039071b8def12
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-04 22:16:41 +0200

    add const parsePaginationParams in utils

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ export const setupServer = () => {
 
     app.get('/', (req, res) => {
         res.json({
-            message: 'Hello world!',
+            message: 'Hello, Владислав Апельганц!',
         });
     });
 
