4bcf8b22b987991c208f225656048b4133a3bf1d
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-05 23:17:16 +0200

    2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,27 +2,25 @@ import { ContactsCollection } from '../db/models/contacts.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
 export const getAllContacts = async ({
-    page,
-    perPage,
+    page = 1,
+    perPage = 10,
 
 }) => {
     const limit = perPage;
     const skip = (page - 1) * perPage;
 
-    const contactsQuery = ContactsCollection.find();
+    //const contactsQuery = ContactsCollection.find();
     const contactsCount = await ContactsCollection.find()
-        .merge(contactsQuery)
+        
         .countDocuments();
 
-    const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+    const contacts = await ContactsCollection.find().skip(skip).limit(limit).exec();
     
     const paginationData = calculatePaginationData({contactsCount,
         perPage, page});
 
     return {
-        contacts,
-        page,
-        perPage,
+        data: contacts,
         ...paginationData,
     };
 };
