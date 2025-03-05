c8bb58d7214705d0132128e70f81692c2ae9cf5d
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-05 22:59:37 +0200

    fixed

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -20,7 +20,9 @@ export const getAllContacts = async ({
         perPage, page});
 
     return {
-       contacts,
+        contacts,
+        page,
+        perPage,
         ...paginationData,
     };
 };
