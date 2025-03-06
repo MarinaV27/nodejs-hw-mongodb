7856613b080b0ca17dc4760eccaf69982917ddb2
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-06 22:40:12 +0200

    .sort({ [sortBy]: sortOrder }) in services/students.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,14 +7,20 @@ import {
     deleteContact
 } from "../services/contacts.js";
 import { parsePaginationParams } from "../utils/parsePaginationParams.js";
-
+import { parseSortParams } from "../utils/parseSortParams.js";
 
 
 
 export const getContactsController = async (req, res) => {
     const { page, perPage } = parsePaginationParams(req.query);
+
+    const { sortBy, sortOrder } = parseSortParams(req.query);
+
     const contacts = await getAllContacts({
-        page, perPage,
+        page,
+        perPage,
+        sortBy,
+        sortOrder,
     });
     
         res.status(200).json({
