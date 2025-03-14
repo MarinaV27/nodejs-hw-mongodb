70f5d8c0ad16c2971344e097796befc0cfd123a9
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-14 22:38:19 +0200

    update ContactSchema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,21 +2,31 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object(
     {
-        name: Joi.string().min(3).max(30).required(),
-        phoneNumber: Joi.string().min(3).max(20).required().pattern(/[+0*9]{3,20}$/),
-        email: Joi.string().min(3).max(20),
+        name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Name must be a string',
+    'string.min': 'Name must have at least {#limit} characters',
+    'string.max': 'Name cannot exceed {#limit} characters',
+    'any.required': 'Name is a required field',
+  }),
+        phoneNumber: Joi.string().required().pattern(/[+0*9]{3,20}$/),
+        email: Joi.string(),
         isFavourite: Joi.boolean(),
-        contactType: Joi.string().min(3).max(20).required(),
+        contactType: Joi.string().valid('work', 'home', 'personal').required(),
 
     });
 
 export const updateContactSchema = Joi.object(
     {
-        name: Joi.string().min(3).max(30),
-        phoneNumber: Joi.string().min(3).max(20).pattern(/[+0*9]{3,20}$/),
-        email: Joi.string().min(3).max(20),
+        name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Name must be a string',
+    'string.min': 'Name must have at least {#limit} characters',
+    'string.max': 'Name cannot exceed {#limit} characters',
+    'any.required': 'Name is a required field',
+  }),
+        phoneNumber: Joi.string().pattern(/[+0*9]{3,20}$/),
+        email: Joi.string(),
         isFavourite: Joi.boolean(),
-        contactType: Joi.string().min(3).max(20),
+        contactType: Joi.string().valid('work', 'home', 'personal').required(),
     });
 
 
