4f7a7e217f24f68103c967aaf23f7b407a90df3a
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-18 23:15:05 +0200

    add router.post('/login',

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,14 +4,21 @@ import { reqisterUserSchema } from "../validation/auth.js";
 import { registerUserController } from "../controllers/auth.js";
 import { validateBody } from '../middlewares/validateBody.js';
 
+import { loginUserSchema } from "../validation/auth.js";
+import { loginUserController } from "../controllers/auth.js";
+
 
 
 const router = Router();
 
-router.post(
-    '/register',
+router.post('/register',
     validateBody(reqisterUserSchema),
     ctrlWrapper(registerUserController),
+);
+
+router.post('/login',
+    validateBody(loginUserSchema),
+    ctrlWrapper(loginUserController),
 );
 
 export default router;
