eebfc0a9590ff6a35dd714906877f5d7898c3f0f
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-18 21:13:31 +0200

    error 409

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,12 @@
 import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 
 import { usersCollection } from "../db/models/user.js";
 
 export const registerUser = async (payload) => {
+    const user = await usersCollection.findOne({ email: payload.email });
+    if (user) throw createHttpError(409, 'Email is use');
+
     const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
     return await usersCollection.create({
