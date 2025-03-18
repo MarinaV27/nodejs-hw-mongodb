c8f9afff13d785bc16fb559e4fa3d34e33b9cfd1
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-18 23:10:52 +0200

    add loginUserSchema

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -5,3 +5,8 @@ export const reqisterUserSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
 });
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
