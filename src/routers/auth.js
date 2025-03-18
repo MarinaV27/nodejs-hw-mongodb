caee56d048a081dc376e402d6c5d7f9f2dae47f6
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-18 21:20:35 +0200

    ls to js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ctrlWraper } from '../utils/ctrlWrapper.js';
-import { reqisterUserSchema } from "../validation/auth.ls";
+import { reqisterUserSchema } from "../validation/auth.js";
 import { registerUserController } from "../controllers/auth.js";
 import { validateBody } from '../middlewares/validateBody.js';
 
