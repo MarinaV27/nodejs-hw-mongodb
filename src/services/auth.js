fd00c0a0992a2eca1b34cb5fbf4e2d747865092a
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-18 21:07:26 +0200

    install bcrypt

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,12 @@
+import bcrypt from 'bcrypt';
+
 import { usersCollection } from "../db/models/user.js";
 
 export const registerUser = async (payload) => {
-    return await usersCollection.create(payload);
+    const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+    return await usersCollection.create({
+        ...payload,
+        password: encryptedPassword,
+    });
 };
