9b4c59be61285c764790ce11b97a91f3a356a27b
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-19 22:26:24 +0200

    add ACCESS_TOKEN_TTL, REFRESH_TOKEN_TTL  in servises/auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,12 @@
+import crypto from "node:crypto";
+
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 
 import { UsersCollection } from "../db/models/user.js";
+import { ACCESS_TOKEN_TTL, REFRESH_TOKEN_TTL } from '../constants/index.js';
+import { SessionsCollection } from '../db/models/session.js';
+
 
 export const registerUser = async (payload) => {
     const user = await UsersCollection.findOne({ email: payload.email });
@@ -17,14 +22,25 @@ export const registerUser = async (payload) => {
 
 export const loginUser = async (payload) => {
     const user = await UsersCollection.findOne({ email: payload.email });
-    if (!user) {
+    if (user === null) {
         throw createHttpError(404, 'User not found');
     }
     const isEqual = await bcrypt.compare(payload.password,
         user.password); // Порівнюємо хеші паролів
     
-    if (!isEqual) {
+    if (isEqual === false) {
         throw createHttpError(401, 'Unauthorized');
     }
+
+    const accessToken = crypto.randomBytes(30).toString('base64');
+    const refreshToken = crypto.randomBytes(30).toString('base64');
+
+    return await SessionsCollection.create({
+        iserId: user._id,
+        accessToken,
+        refreshToken,
+        accessTokenValidUntil: new Date(Date.now() + ACCESS_TOKEN_TTL),
+        refreshTokenValidUntil: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
 
 };
