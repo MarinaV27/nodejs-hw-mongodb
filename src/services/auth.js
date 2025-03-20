45d2fdf25a9371224970887a5bfa131d4f4a6f48
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-20 22:17:30 +0200

    add refreshUsersSession

--- src/services/auth.js
+++ src/services/auth.js
@@ -65,6 +65,30 @@ const createSession = () => {
 
 };
 
-const newSession = createSession();
+export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
+    const session = await SessionsCollection.findOne({
+        _id: sessionId,
+        refreshToken,
+    });
 
-await
+    if (!session) {
+        throw createHttpError(401, 'Session not found');
+    }
+
+    const isSessionTokenExpired = 
+        new Date() > new Date(session.refreshTokenValidUntil);
+    
+    if (isSessionTokenExpired) {
+        throw createHttpError(401, 'Session token expired');
+    }
+
+    const newSession = createSession();
+    
+    await SessionsCollection.deleteOne({ _id: sessionId, refreshToken });
+
+    return await SessionsCollection.create({
+        userId: session.userId,
+        ...newSession,
+    });
+
+};
