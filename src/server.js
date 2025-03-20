94f65661419b5bba6efe184b54b28712d66e10da
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-20 23:50:03 +0200

    add app.use(express.json());

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ export const setupServer = () => {
     const app = express();
 
 
-    //app.use(express.json());
+    app.use(express.json());
     app.use(cors());
 
     app.use(cookieParser());
