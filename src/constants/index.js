e0d63581e80d31f5e80f7ef2ea2a70f526569e60
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-26 22:48:03 +0200

    const SMTP

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,6 +7,10 @@ export const SORT_ORDER = {
 export const ACCESS_TOKEN_TTL = 15 * 60 * 1000;  //TTL (time to live) час життя   15 хв
 export const REFRESH_TOKEN_TTL = 30 * 24 * 60 * 60 * 1000;  //30 днів
 
-export const ROLES = {
-
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
 };
