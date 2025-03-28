1012e767776d7740f4df6751f2ef9d4000a2a962
MarinaV27 <Vasilishinamari@gmail.com>
2025-03-28 22:38:39 +0200

    add const templateSource

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,10 +4,14 @@ import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import jwt from 'jsonwebtoken';
 
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 import { UsersCollection } from "../db/models/user.js";
 import { ACCESS_TOKEN_TTL, REFRESH_TOKEN_TTL } from '../constants/index.js';
 import { SessionsCollection } from '../db/models/session.js';
-import { SMTP } from "../constants/index.js";
+import { SMTP, TEMPLATES_DIR } from "../constants/index.js";
 import { getEnvVar } from "../utils/getEnvVar.js";
 import { sendEmail } from "../utils/sendMail.js";
 
@@ -115,12 +119,28 @@ export const requestResetToken = async (email) => {
     {
       expiresIn: '5m',
     },
+    );
+    
+     const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password-email.html',
   );
+
+  const templateSource = (
+    await fs.readFile(resetPasswordTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.name,
+    link: `${getEnvVar('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+  });
 
    await sendEmail({
     from: getEnvVar(SMTP.SMTP_FROM),
     to: email,
     subject: 'Reset your password',
-    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    html,
   });
 };
+
