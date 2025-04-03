b0ca74e0e2f6b7512df472480e83180a98863d6c
MarinaV27 <Vasilishinamari@gmail.com>
2025-04-03 23:11:23 +0300

    10

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const SORT_ORDER = {
 };
 
 
-export const ACCESS_TOKEN_TTL = 190 * 60 * 1000;  //TTL (time to live) час життя   15 хв
+export const ACCESS_TOKEN_TTL = 15 * 60 * 1000;  //TTL (time to live) час життя   15 хв
 export const REFRESH_TOKEN_TTL = 30 * 24 * 60 * 60 * 1000;  //30 днів
 
 export const SMTP = {
