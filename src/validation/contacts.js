0a4f77ff0ba9f815fef30aa02a6d3ab8b122eba6
MarinaV27 <Vasilishinamari@gmail.com>
2025-04-06 22:53:01 +0300

    fixed updateContactSchema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -10,11 +10,16 @@ export const createContactSchema = Joi.object(
         contactType: Joi.string().valid('work', 'home', 'personal'),
     });
 
-export const updateContactSchema = createContactSchema.fork(
-  ['name', 'phoneNumber', 'contactType'],
-  (field) => field.required(),
-);
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string().min(8).max(20),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+}).min(1); // потрібно хоча б одне поле
 
+//min(1) => Дозволяє оновлювати будь-яке з полів
 
+// {} => Забороняє надсилати повністю порожній об’єкт 
 
         
