e688ca04672aae470ff270b62292a59be941d904
MarinaV27 <149487248+MarinaV27@users.noreply.github.com>
2025-04-07 16:37:51 +0300

    add swagger/paths/students/post.yaml

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,11 +3,16 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object(
     {
-        name: Joi.string().min(3).max(30),
-        phoneNumber: Joi.string().min(8).max(20),
+        name: Joi.string().min(3).max(30).required().messages({
+          'string.base': 'Username should be a string',
+          'string.min': 'Username should have at least {#limit} characters',
+          'string.max': 'Username should have at most {#limit} characters',
+          'any.required': 'Username is required',
+        }),
+        phoneNumber: Joi.string().min(8).max(20).required(),
         email: Joi.string().email(),
         isFavourite: Joi.boolean(),
-        contactType: Joi.string().valid('work', 'home', 'personal'),
+        contactType: Joi.string().valid('work', 'home', 'personal').required(),
     });
 
 export const updateContactSchema = Joi.object({
